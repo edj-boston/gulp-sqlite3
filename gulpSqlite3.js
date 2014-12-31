4e49d758abb237ef0e6e937150b3d30178fc784a
Eric Johnson <edj.boston@gmail.com>
2014-12-31 01:32:53 -0800

    Initial commit of module

---
+++ gulpSqlite3.js
@@ -0,0 +1,42 @@
+// External dependancies
+var gutil = require('gulp-util'),
+	through = require('through2'),
+	sqlite3 = require('sqlite3');
+
+// Setup
+const PLUGIN_NAME = 'gulp-sqlite3';
+
+
+// 
+function gulpSqlite3(filename) {
+
+	// Validation
+	if( !filename ) throw new gutil.PluginError(PLUGIN_NAME, 'missing filename!');
+
+	// Set the verbosity to high
+	sqlite3.verbose();
+
+	// Connect to the database
+	var db = new sqlite3.Database(filename, null, function(err, something) {
+		if(err) throw new gutil.PluginError(PLUGIN_NAME, err);
+		gutil.log(PLUGIN_NAME, 'successfully connect to database', filename);
+	});
+
+	// Stream handler
+	return through.obj(function(file, enc, cb) {
+		
+		if( file.isBuffer() ) {
+			db.exec(file.contents.toString(enc), function(err, str) {
+				if(err) throw new gutil.PluginError(PLUGIN_NAME, err);
+				gutil.log(PLUGIN_NAME, 'successfully processed', file.relative);
+				cb(null, file);
+			});
+		} else {
+			cb(null, file);
+		}
+
+	});
+
+}
+
+module.exports = gulpSqlite3;
